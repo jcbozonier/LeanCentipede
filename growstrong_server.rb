56c301bcc31fd3a4eeeb6af182f538ccde0e0c41
Justin Bozonier <darkxanthos@gmail.com>
2012-04-21 19:22:32 -0400

    added bare bones site.

--- growstrong_server.rb
+++ growstrong_server.rb
@@ -14,3 +14,11 @@ set :views, "./views"
 get "/" do
   erb :welcome
 end
+
+get "/interviewee_sign_up" do
+  erb :interviewee_sign_up
+end
+
+get "/interviewee_sign_up_submitted" do
+  erb :interviewee_sign_up_submitted
+end
