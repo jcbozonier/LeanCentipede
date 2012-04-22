db5ea040a51cf59d9cc64dfb93c2e6004deb44e6
Justin Bozonier <darkxanthos@gmail.com>
2012-04-21 20:43:46 -0400

    starting to correct view to have correct verbiage.

---
+++ validator_server.rb
@@ -0,0 +1,15 @@
+get "/interviewer_sign_up" do
+  erb :interviewer_sign_up
+end
+
+get "/search_for_validation" do
+  erb :search_for_validation
+end
+
+post "/interviewer_sign_up_submitted" do
+  erb :interviewer_sign_up_submitted
+end
+
+get "/check_validation_interview_order" do
+  erb :check_validation_interview_order
+end
