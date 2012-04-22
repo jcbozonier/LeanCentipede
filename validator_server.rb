ca542b6b6667b9247bc8ea3be9f09461e66fc597
Keith <keith@moran-obrien.com>
2012-04-21 20:57:37 -0400

    global variables and validation form submission

--- validator_server.rb
+++ validator_server.rb
@@ -1,3 +1,5 @@
+$requested_profiles = Array.new
+
 get "/interviewer_sign_up" do
   erb :interviewer_sign_up
 end
@@ -11,5 +13,19 @@ post "/interviewer_sign_up_submitted" do
 end
 
 get "/check_validation_interview_order" do
+
+  profile = {}
+  profile[:gender] = params[:gender]
+  profile[:age] = params[:age]
+  profile[:income] = params[:income]
+  profile[:products] = params[:products]
+  profile[:expertise] = params[:expertise]
+  profile[:adoption] = params[:adoption]
+  profile[:voice_call] = params[:voice_call]
+
+  $requested_profiles << profile
+
+  puts $requested_profiles
+
   erb :check_validation_interview_order
 end
